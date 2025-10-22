12ba79825d56bdcd08a1c97c846a6af9a248b4f0
jordanjordanjordan0 <jhill011@boyslatin.net>
2025-10-22 15:11:38 +0000

    10/22 work

--- unit 2/exercise2.py
+++ unit 2/exercise2.py
@@ -3,25 +3,19 @@ import nfl_data_py as nfl
 
 from helperFunctions import get_team_records, get_season_Results_By_team
 
-schdules = get_team_records(2025)
-print(schdules)
+#schdules = get_team_records(2025)
+#print(schdules)
 
-top6_Teams = ('TB','IND','LA','BUF','SF','SEA','PIT')
+#top6_Teams = ('TB','IND','LA','BUF','SF','SEA','PIT')
 
 
-team_1 = get_season_Results_By_team(2025,'TB')
-team_2 = get_season_Results_By_team(2025,'IND')
-team_3 = get_season_Results_By_team(2025,'LA')
-team_4 = get_season_Results_By_team(2025,'BUF')
-team_5 = get_season_Results_By_team(2025,'SF')
-team_6 = get_season_Results_By_team(2025,'SEA')
+#team_1 = get_season_Results_By_team(2025,'TB')
+#team_2 = get_season_Results_By_team(2025,'IND')
+#team_3 = get_season_Results_By_team(2025,'LA')
+#team_4 = get_season_Results_By_team(2025,'BUF')
+#team_5 = get_season_Results_By_team(2025,'SF')
+#team_6 = get_season_Results_By_team(2025,'SEA')
 
-print(team_1)
-print(team_2)
-print(team_3)
-print(team_4)
-print(team_5)
-print(team_6)
 
 
 
@@ -29,15 +23,24 @@ print(team_6)
 
 
 def pdCheck():
-    print("please enter a number. Please enter 'q' to calculate")
-    number = int(input())
+    print("please enter a number")
+    number = input()
     values = []
-    while calculate = 'q':
-        values.append(number)
+
+    while number != 'q':
+        values.append(int(number))
         print(values)
-        print("please enter a number")
+        print("Please enter a number")
         number = input()
     else:
-        print('doing calculation')
+        print('doing calculation...')
+        total = sum(values)
+        print(total)
+
+
+# The best home point difference this season is INDY 64
+
+# the best away point difference this season is 
+        
 
 pdCheck()
---
+++ unit 2/exercise3.py
@@ -0,0 +1,12 @@
+import pandas as pdimport 
+nfl_data_py as nfldef get_team_records(year):    
+games = nfl.import_schedules([year])
+
+
+# 1- 1 - Justin Herbert (67.5) 2 - Dak Prescott (71.4) 3 - Matthew Stafford (66.1) 4 - Patrick Mahomes (66.1) 5 - Daniel Jones (71)
+
+# 2 The highest cmp pt can tell you that they have a good playbook and are well adapt to their team.
+
+# 3 Didnt say which week in specific
+
+# 4 it could mean that he breaks away for big runs but on average he doesnt get a lot of yards.
