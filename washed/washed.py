824fcae34a53db27edcf69cf03eca654268a7834
jordanjordanjordan0 <jhill011@boyslatin.net>
2025-12-05 16:15:25 +0000

    WORK

---
+++ washed/washed.py
@@ -0,0 +1,11 @@
+from helperFunctions import get_season_totals_by_positions, plot_positions_stat_bar, plot_player_stat_by_week, get_player_stats
+
+
+
+# 1 - For a player to be washed to me. Their stats would have to drop off and decrease. So lets say QB
+
+# QB Passing Yards
+# Qb Passer Ratings
+# Qb Passing Touchdowns
+# Qb interceptions
+
