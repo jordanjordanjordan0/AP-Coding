c3be69caf34bc83912f4e55d0915381fe32bd8df
jordanjordanjordan0 <jhill011@boyslatin.net>
2025-12-11 16:13:44 +0000

    12/11 work

---
+++ ratings/ratings.py
@@ -0,0 +1,6 @@
+# 1 - Being underrated is under recongnized for something your good. Being overrated is to not live up to the hype and standard people are setting for you
+#     Three stats i would i9 how many games did they play compare to the best stats they put up And how good are their averages.
+
+
+
+# 2 - Baker Mayfield is perfectly rated. Tua Is Overrated due to how bad he plays. Brock is underrated due to how the 49ers have been playing recently. Amon Is perfect. CeeDee is perfect
